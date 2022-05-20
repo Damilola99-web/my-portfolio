5eb244b67ec680ce5b8183cb9eca97946211e6d6
Abdul-rasheed <rasheedw622@gmail.com>
2022-05-20 09:58:29 +0100

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className='flex items-center justify-center py-4 bg-[#081420] text-white' >
+      <p> &copy; copyright 2022 <span className='text-pink-600'>Abdul-Rasheed</span></p>
+    </div>
+  );
+}
+
+export default Footer;
